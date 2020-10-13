c745c3caf74a0b5ba03cf754f13e62ab8d63e56f
VieuL <loicvieu@outlook.fr>
2020-10-13 11:40:08 +0200

    Automatisation de l'ip

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.rmi.*;
@@ -28,7 +29,7 @@ public class Client {
                 Remote r = Naming.lookup("rmi://localhost:1099/Switcher");
                 this.startListen(this.port);
                 if (r instanceof Machine) {
-                    ((Machine) r).read(name, "127.0.0.1", this.port);
+                    ((Machine) r).read(name, InetAddress.getLocalHost().getHostAddress(), this.port);
                 }
                 //On att le résultat
                 this.waitMessage();
@@ -41,8 +42,13 @@ public class Client {
 
 
     public static <Information> void main(String[] args) throws IOException {
-        Client client1 = new Client(8080);
-
+        int port;
+        try{
+             port = Integer.parseInt(args[0]);
+        }catch (Exception e){
+             port = 8080;
+        }
+        Client client1 = new Client(port);
 
         String result = client1.read("ressource_1.txt");
         System.out.println(result);
